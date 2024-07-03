1903b4875f4b80a6a6e1a62479a4c107da907a5c
Ernis1015 <camca.imba05@gmail.com>
2024-07-03 13:55:56 +0600

    dz-6

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserList from './pages/UsersList.tsx'; 
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>User List</h1>
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
